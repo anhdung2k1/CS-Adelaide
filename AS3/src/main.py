6bb4c8e2d31970a9e5e0f0f3b5c495bdeb7de929
anhdung2k1 <19521167@gm.uit.edu.vn>
2023-11-15 09:11:28 +0700

    AS3 RNN stock price binance

--- AS3/src/main.py
+++ AS3/src/main.py
@@ -6,18 +6,26 @@ root_directory = os.path.dirname(file_location)  # Get root dir
 sys.path.append(os.path.join(root_directory, '..'))
 from tools.scraping_data.scraping_bitcoin_RNN import *
 
+# Params
+_start = Date(2022, 8, 17, 0)
+_end = Date(2023, 11, 14, 0)
+
+
+def scraping_data_export(start, end, df) -> pd.DataFrame:
+    begin_date = start.get_timestamp()
+    end_date = end.get_timestamp()
+    print(begin_date, end_date)
+    request_num = len([k for k in range(begin_date, end_date, INTERVAL_MS)])
+    print(f'Request Data: {request_num}')
+    print(f'Export Data with row x cols: {df.shape}')
+    dataset_file_dir = os.path.join(root_directory, '..', '..', 'dataset', 'BTCUSDT.xlsx')
+    export_xlsx(dataset_file_dir, df)
+
 
 def main():
-    start = Date(2022, 3, 1, 0)
-    end = Date(2023, 11, 14, 0)
-    start.print()
-    print(start.get_timestamp())
-    end.print()
-    print(end.get_timestamp())
-    # Print request numbers
-    print("\n%d request number\n" % len([k for k in range(start.get_timestamp(), end.get_timestamp(), INTERVAL_MS)]))
-    df = get_data_klines(start, end)
-    export_xlsx(os.path.join(root_directory, '..', '..', 'dataset', 'BTCUSDT.xlsx'), df)
+    df1 = get_data_klines(_start, _end)
+    # df2 = get_data_trend(_start, _end)
+    scraping_data_export(_start, _end, df1)
 
 
 if __name__ == "__main__":
--- AS3/tools/scraping_data/scraping_bitcoin_RNN.py
+++ AS3/tools/scraping_data/scraping_bitcoin_RNN.py
@@ -2,23 +2,24 @@
 import datetime
 import sys
 import os
-file_location = os.path.abspath(__file__) # Get current file abspath
-root_directory = os.path.dirname(file_location) # Get root dir
+import pandas as pd
+file_location = os.path.abspath(__file__)  # Get current file abspath
+root_directory = os.path.dirname(file_location)  # Get root dir
 sys.path.append(os.path.join(root_directory))
-from utils.TrendReq import *
 from binance.spot import Spot as Client
 from utils.secrets import *
+
 # Set up client connector
 # RSA keys set up
 [api_key, api_secret] = get_api_key()
 client = Client(api_key, api_secret)
-pytrend = TrendReq()
 # Set up params
 PAIR = "BTCUSDT"  # Pair Binance API query
 INTERVAL = "1h"  # Interval Binance API query
-INTERVAL_MS = int(60 * 60 * 60)
-BINANCE_HEADER = ["Open Time", "Open Price", "High Price", "Low Price", "Close Price", "Volume",
-                  "Close Time", "Quote Asset Volume", "Number of Trades", "Taker Buy Base Asset Volume", "Taker Buy Quote Asset Volume", "Ignore"]
+INTERVAL_MS = int(60 * 60 * 1e6)
+BINANCE_HEADER = ["Open time", "Open price", "High price", "Low price", "Close price", "Volume", "Close time",
+                  "Quote asset volume", "Number of trades", "Taker buy base asset vol", "Taker buy quote asset vol",
+                  "Ignore"]  # Dataframe header
 KW_LIST = ["Bitcoin"]  # KW used by Google trend API
 
 
@@ -35,91 +36,114 @@ class Date:
         self.min = 0 if min is None else min
 
     def get_timestamp(self):
-        return int(datetime.datetime(self.y, self.m, self.d, self.h, self.min).timestamp() * 1000)
+        return int(float(datetime.datetime(self.y, self.m, self.d, self.h, self.min).strftime('%s.%f'))*1000)
 
     def print(self):
         print("{}/{:02d}/{:02d} - {:02d}:{:02d}".format(self.y, self.m, self.d, self.h, self.min))
 
 
 def get_klines(start, end) -> pd.DataFrame:
-    return pd.DataFrame(client.klines(PAIR, INTERVAL, limit=1000, startTime=start, endTime=end),
-                        columns=BINANCE_HEADER)
-
-
-def _check_data(data_frame) -> pd.DataFrame:
     """
-    Check and fix open time data
+    Fetch Binance Klines. Start and end date must represent at most one thousand interval time.
+
+    Arguments :
+        start -- Date
+        end   -- Date
+
+    Return
+        df    -- pd.DataFrame
+    """
+    return pd.DataFrame(client.klines(PAIR, INTERVAL, limit=1000, startTime=start, endTime=end), columns=BINANCE_HEADER)
+
+
+def _check_data(df) -> pd.DataFrame:
+    """
+    Check and fix Open time data .
+
+    Arguments :
+        df    -- pd.DataFrame
+
+    Return
+        df    -- pd.DataFrame
+
     """
 
-    for i in range(len(data_frame.index) - 1):
-        o1 = data_frame["Open Time"][i] + int(INTERVAL_MS / 1e3)
-        o2 = data_frame["Open Time"][i + 1]
+    for i in range(len(df.index) - 1):
+        o1 = df["Open time"][i] + int(INTERVAL_MS / 1e3)
+        o2 = df["Open time"][i + 1]
 
-        if o1 != o2:  # Check if i and i+1 data are consistent
-            s = data_frame.copy().xs(i)
-            for inter in range(o1, o2, int(INTERVAL_MS / 1e3)):
-                s["Open Time"] = inter
-                for c in ["Volume", "Quote Asset Volume", "Number of Trades", "Taker Buy Base Asset Volume",
-                          "Taker Buy Quote Asset Volume", "Close Time"]:
-                    if c in data_frame.columns and c != "Close Time":
+        if o1 != o2:  # Check if the i and i+1 data are consistent
+            s = df.copy().xs(i)
+            for inter in range(o1, o2, int(INTERVAL_MS / 1e3)):  # Loop to prevent multiple missing data
+                s["Open time"] = inter
+                for c in ["Volume", "Quote asset volume", "Number of trades", "Taker buy base asset vol",
+                          "Taker buy quote asset vol", "Close time"]:
+                    if c in df.columns and c != "Close time":
                         s[c] = 0
-                    elif c in data_frame.columns and c == "Close Time":
+                    elif c in df.columns and c == "Close time":
                         s[c] = inter - 1 + int(INTERVAL_MS / 1e3)
-                data_frame = pd.concat(
-                    [data_frame[:i], pd.DataFrame(dict(s), columns=data_frame.columns, index=[len(data_frame.columns)]),
-                     data_frame[i:]])
-    return data_frame.sort_values(by=["Open Time"]).reset_index(drop=True)
+                df = pd.concat([df[:i], pd.DataFrame(dict(s), columns=df.columns, index=[len(df.columns)]), df[i:]])
+
+    return df.sort_values(by=["Open time"]).reset_index(drop=True)
 
 
 def get_data_klines(start, end) -> pd.DataFrame:
     """
-    Get k_lines calls to fetch Binance Klines data from start to end date
-    """
-    begin_date = start.get_timestamp()
-    end_date = end.get_timestamp()
-    data_frame = pd.DataFrame([], columns=BINANCE_HEADER)
+    Multiple get_klines calls (caused by the API limitation) to fetch Binance Klines data from start date to end date.
 
-    for time in range(begin_date, end_date, INTERVAL_MS):
-        if time + INTERVAL_MS <= end_date:
-            data_frame = pd.concat([data_frame, get_klines(time, time + INTERVAL_MS)], ignore_index=True)
+    Arguments :
+        start -- Date
+        end   -- Date
+
+    Return
+        df    -- pd.DataFrame
+    """
+    s = start.get_timestamp()
+    e = end.get_timestamp()
+    df = pd.DataFrame([], columns=BINANCE_HEADER)
+
+    for t in range(s, e, INTERVAL_MS):
+        if t + INTERVAL_MS <= e:
+            df = pd.concat([df, get_klines(t, t + INTERVAL_MS)], ignore_index=True)
         else:
-            data_frame = pd.concat([data_frame, get_klines(time, end_date)], ignore_index=True)
-        return _check_data(data_frame.drop_duplicates(subset=["Open Time"]).reset_index(drop=True))
+            df = pd.concat([df, get_klines(t, e)], ignore_index=True)
 
-
-def get_data_trend(start, end) -> pd.DataFrame:
-    """"
-    Fetch Google Search trend data
-    """
-
-    data_frame = pytrend.get_historical_interest(KW_LIST, start.y, start.m, start.d, start.h, end.y,
-                                                 end.m, end.d, end.h)
-    data_frame["Open Time"] = [int(float(df.strftime('%s.%f')) * 1000) for df in data_frame.index]
-    return _check_data(data_frame.drop_duplicates(subset=["Open Time"]).reset_index(drop=True))
+    return _check_data(df.drop_duplicates(subset=["Open time"]).reset_index(drop=True))
 
 
 def test_time_dfs(df1, df2):
     """
-    Checking the correlation between each "Open Time" data from two dfs.
-    Print the row where the error happen if there is one
-    """
+    Checking the correlation between each "Open time" data from two dfs.
+    Print the row where the error happen if there is one.
 
+    Arguments :
+        df1   -- pd.DataFrame
+        df2   -- pd.DataFrame
+
+    """
     size = len(df2.index) if len(df2.index) < len(df1.index) else len(df1.index)
     for i in range(size):
-        o1 = int(df1["Open Time"][i])
-        o2 = int(df2["Open Time"][i])
-        if o1 != o2:
+        o1 = int(df1["Open time"][i])
+        o2 = int(df2["Open time"][i])
+        if (o1 != o2):
             print(i, o1, o2)
             break
 
 
 def formatting(dfs, cols) -> pd.DataFrame:
     """
-    Formatting the final Data Frame. Dfs is a list with one or multiple DataFrame to be concatenated/
-    Cols is a list of the columns required in the final DataFrame
-    """
+    Formatting the final DataFrame. Dfs is a list with one or multiple DataFrame to be concatenated.
+    Cols is a list of the columns required in the final DataFrame.
 
-    final_df = pd.DataFrame
+    Arguments :
+        dfs     -- pd.DataFrame
+        cols    -- list
+    Return
+        df      -- pd.DataFrame
+
+    """
+    final_df = pd.DataFrame()
+
     for df in dfs:
         for col in df.columns:
             if col in cols and col not in final_df.columns:
